1261d29b8987a49536693f9000e199c1af572f21
bingo <121978009+debassser@users.noreply.github.com>
2023-03-17 14:15:03 +0000

    Add files via upload

---
+++ ASCII 256 rep/Program.cs
@@ -0,0 +1,13 @@
+﻿int asciiValue;
+int i = 0;
+for (asciiValue = 0; asciiValue <= 128; asciiValue++)
+{
+    if ((i % 5) == 0)
+    {
+        Console.Write("\n");
+    }
+    Console.Write(" " + Convert.ToChar(asciiValue));
+    i++;
+}
+Console.WriteLine("\n Press enter to exit...");
+Console.ReadLine();
